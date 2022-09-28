3545c935a96905e97cb209a844f0507eac2963cd
sofiapkk <112013205+sofiapkk@users.noreply.github.com>
2022-09-28 16:55:32 -0300

    ASDF

---
+++ portfolio/FrontEnd/src/app/components/hss/hss.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hss',
+  templateUrl: './hss.component.html',
+  styleUrls: ['./hss.component.css']
+})
+export class HssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
