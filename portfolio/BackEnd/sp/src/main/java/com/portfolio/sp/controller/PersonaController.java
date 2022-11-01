43818cc71f96a49b26baf5f5d785bfb7ba49ec9c
sofiapkk <112013205+sofiapkk@users.noreply.github.com>
2022-10-31 21:49:54 -0300

    si

--- portfolio/BackEnd/sp/src/main/java/com/portfolio/sp/controller/PersonaController.java
+++ portfolio/BackEnd/sp/src/main/java/com/portfolio/sp/controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
-@CrossOrigin(origins = "http://localhost:4002")
+@CrossOrigin(origins = "http://localhost:8080")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
    
